57168af5f62501a29646c86dd7ceb83754425b9c
Ryan Legler <ryan.legler@gmail.com>
2023-04-13 20:03:44 -0700

    chore: adds metadata - openGraph and favicon image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,32 @@
 import Script from "next/script";
+import { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
-    title: "Aldercreast",
+export const metadata: Metadata = {
+    title: "Aldercrest Massage + Facials",
+    openGraph: {
+        images: [
+            {
+                url: "./aldercrest.svg",
+                width: 413,
+                height: 523,
+            },
+        ],
+    },
+    icons: {
+        icon: "./favicon.ico",
+        shortcut: {
+            url: "./favicon-32x32.png",
+            sizes: "32x32",
+        },
+        apple: {
+            url: "./apple-touch-icon.png",
+            sizes: "180x180",
+        },
+    },
     description:
         "I am a massage therapist and a holistic esthetician in Milwaukie Oregon. Since 2005 I have been specializing in Swedish, deep tissue, trigger point, and prenatal massage. In 2008, I became a certified Dr. Hauschka esthetician and currently offer holistic facials utilizing the Evan Healy product line. I enjoy the work that I do and the joy it brings to my clients. I look forward to working with you.",
 };
-
-const trackingID = process.env.GOOGLE_ANALYTICS || "";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
@@ -18,24 +37,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     strategy="afterInteractive"
                     src={`https://beamanalytics.b-cdn.net/beam.min.js`}
                 />
-                {/* <Script
-                    strategy="afterInteractive"
-                    src={`https://www.googletagmanager.com/gtag/js?id=${trackingID}`}
-                />
-                <Script
-                    id="data-partytown-config"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', '${trackingID}', {
-                                page_path: window.location.pathname,
-                            });
-                        `,
-                    }}
-                /> */}
             </head>
             <body className="flex justify-center w-full">
                 <div className="flex max-w-[1200px] flex-col w-full px-0 sm:px-8">{children}</div>
