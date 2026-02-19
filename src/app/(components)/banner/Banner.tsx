f782a4d7c0af8ed4506f23d9f84f1a5deb7c869d
Ryan Legler <ryan.legler@gmail.com>
2026-02-19 08:01:22 -0800

    Fixes banner text for proper apostrophe use.
    Corrects the apostrophe usage in the banner text.

--- src/app/(components)/banner/Banner.tsx
+++ src/app/(components)/banner/Banner.tsx
@@ -7,7 +7,7 @@ function Banner() {
             <StyledBannerContent>
                 <Truck className="h-5 w-5 shrink-0" aria-hidden />
                 <p className="font-semibold">
-                    We're moving to Oregon City! We will be closed until March 17, at which point we
+                    We&apos;re moving to Oregon City! We will be closed until March 17, at which point we
                     will reopen at our new location.
                 </p>
             </StyledBannerContent>
